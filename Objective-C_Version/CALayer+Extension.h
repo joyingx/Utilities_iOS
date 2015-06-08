fefc8dd5434010d341b0752a0ca2465466c66b27
Joyingx <joyingx@gmail.com>
2015-06-08 14:06:44 +0800

    add CALayer+Extension

---
+++ Objective-C_Version/CALayer+Extension.h
@@ -0,0 +1,29 @@
+//
+//  CALayer+Extension.h
+//  UtilitiesDemo
+//
+//  Created by joyingx on 15/6/4.
+//  Copyright (c) 2015年 joyingx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface CALayer (Extension)
+
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+@property (nonatomic) CGPoint center;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize  size;
+
+- (UIImage *)snapshotImage;
+- (void)setShadow:(UIColor*)color offset:(CGSize)offset radius:(CGFloat)radius;
+
+@end
---
+++ Objective-C_Version/CALayer+Extension.m
@@ -0,0 +1,143 @@
+//
+//  CALayer+Extension.m
+//  UtilitiesDemo
+//
+//  Created by joyingx on 15/6/4.
+//  Copyright (c) 2015年 joyingx. All rights reserved.
+//
+
+#import "CALayer+Extension.h"
+
+@implementation CALayer (Extension)
+
+- (UIImage *)snapshotImage {
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0);
+    [self renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+- (void)setShadow:(UIColor *)color offset:(CGSize)offset radius:(CGFloat)radius {
+    self.shadowColor        = color.CGColor;
+    self.shadowOffset       = offset;
+    self.shadowRadius       = radius;
+    self.shadowOpacity      = 1;
+    self.shouldRasterize    = YES;
+    self.rasterizationScale = [UIScreen mainScreen].scale;
+}
+
+#pragma mark - Getter Methods
+- (CGFloat)left {
+    return self.frame.origin.x;
+}
+
+- (CGFloat)top {
+    return self.frame.origin.y;
+}
+
+- (CGFloat)right {
+    return self.frame.origin.x + self.frame.size.width;
+}
+
+- (CGFloat)bottom {
+    return self.frame.origin.y + self.frame.size.height;
+}
+
+- (CGFloat)width {
+    return self.frame.size.width;
+}
+
+- (CGFloat)height {
+    return self.frame.size.height;
+}
+
+- (CGFloat)centerX {
+    return self.left + self.width / 2.0;
+}
+
+- (CGFloat)centerY {
+    return self.top + self.height / 2.0;
+}
+
+- (CGPoint)center {
+    return CGPointMake(self.centerX, self.centerY);
+}
+
+- (CGPoint)origin {
+    return self.frame.origin;
+}
+
+- (CGSize)size {
+    return self.frame.size;
+}
+
+#pragma mark - Setter Methods
+- (void)setLeft:(CGFloat)left {
+    CGRect frame   = self.frame;
+    frame.origin.x = left;
+    self.frame     = frame;
+}
+
+- (void)setTop:(CGFloat)top {
+    CGRect frame   = self.frame;
+    frame.origin.y = top;
+    self.frame     = frame;
+}
+
+- (void)setRight:(CGFloat)right {
+    CGRect frame   = self.frame;
+    frame.origin.x = right - self.width;
+    self.frame     = frame;
+}
+
+- (void)setBottom:(CGFloat)bottom {
+    CGRect frame   = self.frame;
+    frame.origin.y = bottom - self.height;
+    self.frame     = frame;
+}
+
+- (void)setWidth:(CGFloat)width {
+    CGRect frame     = self.frame;
+    frame.size.width = width;
+    self.frame       = frame;
+}
+
+- (void)setHeight:(CGFloat)height {
+    CGRect frame      = self.frame;
+    frame.size.height = height;
+    self.frame        = frame;
+}
+
+- (void)setCenterX:(CGFloat)centerX {
+    CGRect frame   = self.frame;
+    frame.origin.x = centerX - self.width / 2.0f;
+    self.frame     = frame;
+}
+
+- (void)setCenterY:(CGFloat)centerY {
+    CGRect frame   = self.frame;
+    frame.origin.y = centerY - self.height / 2.0f;
+    self.frame     = frame;
+}
+
+- (void)setCenter:(CGPoint)center {
+    CGRect frame   = self.frame;
+    frame.origin.x = center.x - self.width / 2.0f;
+    frame.origin.y = center.y - self.height / 2.0f;
+    self.frame     = frame;
+}
+
+- (void)setOrigin:(CGPoint)origin {
+    CGRect frame = self.frame;
+    frame.origin = origin;
+    self.frame   = frame;
+}
+
+- (void)setSize:(CGSize)size {
+    CGRect frame = self.frame;
+    frame.size   = size;
+    self.frame   = frame;
+}
+
+@end
